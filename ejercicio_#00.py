a4cf13d0799cc16e90759b1e23c689f06fa8477c
Andres Bravo <andres01032001@gmail.com>
2026-03-07 12:26:13 -0500

    se creo el primer ejercicio del reto

---
+++ ejercicio_#00.py
@@ -0,0 +1,19 @@
+# documentacion python: https://docs.python.org/es/3.14/
+
+# variables
+lenguaje = 'Python'
+nombre = 'andres'
+edad = 25
+
+# CONSTANTE: python no maneja constantes pero es una buena practica utilizar variables en mayusculas para dar a entender que se trata de una variable del cual su valor no cambiara 
+
+CONTRASEÑA = 123456789
+
+# tipos de datos 
+valor = 1  # int
+apellido = 'Bravo' # str
+eres_mayor_edad = True # boleanos
+precio = 34.3 # float
+
+# imprimir por consola
+print(f'hola {lenguaje}')
