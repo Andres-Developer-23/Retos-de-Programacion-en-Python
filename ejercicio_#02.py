ccd0c59d0af5ae94d874b485d850bfdba1fec5f0
Andres Bravo <andres01032001@gmail.com>
2026-03-09 15:18:08 -0500

    ejercicio #02 sobre funciones y alcanse

---
+++ ejercicio_#02.py
@@ -0,0 +1,45 @@
+# funciones en python
+
+####### estructura basica #########
+
+# esta funcion resive un parametro (nombre)
+def saludar(nombre):
+    return f'hola {nombre}'
+
+# guardar el resultado de la funcion   
+mensaje = saludar('andres')
+
+# ejecutar la funcion
+print(mensaje)
+
+######################################
+# argumentos predefinidos
+
+def suma(a, b=3):
+    return f'{a} + {b} = {a + b}'
+
+resultado = suma(3)
+print(resultado)
+
+#################################
+# funciones lambdas o funciones anonimas
+
+restar = lambda a, b: a - b
+print(restar(6, 5)) 
+
+###################################
+# manejo de argumentos variables
+# *args: Recibe múltiples argumentos como una tupla.
+# **kwargs: Recibe múltiples argumentos con nombre como un diccionario.
+
+def pedido(tamaño, *ingredientes, **detalles):
+    print(f'pizza tamaño {tamaño} con ingredientes {ingredientes}')
+    print(f'detalles {detalles}')
+
+pedido("Grande", "Peperoni", "Queso", direccion="Calle 123", nota="Sin cebolla")
+
+def sumar(b, *numero):
+    """esta funcion rescive primero un parametro (b) y lo suma con los siguientes argumentos multiples(*numero) estos los resive la funcion en forma de tupla y los recore uno a uno utilizando un ciclo for"""
+    for numer in numero:
+        print(f'{b} + {numer} = {b + numer}')
+sumar(1,1,2,3,4,5,6,7,8,9,10)
